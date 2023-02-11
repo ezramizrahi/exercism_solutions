522f01c73c8801c3c8ddc4b0bfd7b7deeb545427
ezramizrahi <mizrahi.ez@gmail.com>
2023-02-11 17:37:54 +1100

    first commit

---
+++ clojure/sublist/src/sublist.clj
@@ -0,0 +1,11 @@
+(ns sublist)
+
+(defn sublist? [a b]
+  (some #{a} (partition (count a) 1 b)))
+
+(defn classify [list1 list2]
+  (cond
+    (= list1 list2) :equal
+    (sublist? list1 list2) :sublist
+    (sublist? list2 list1) :superlist
+    :default :unequal))
